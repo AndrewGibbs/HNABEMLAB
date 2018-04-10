b105b77a8ec663ed2bf0daae8f30393c3abf08e8
Unknown <andrewgibbs1987@gmail.com>
2018-04-10 17:12:13 +0200

    Ready to merge with master to create simplified version of HNABEMLAB, which uses NSD45

--- addPaths.m
+++ addPaths.m
@@ -2,13 +2,14 @@ function addPaths(  )
 %Adds all the paths to subfolders required for HNA BEM LAB
 addpath ../Basis;
 addpath ../BoundaryFunctions;
-addpath ../BoundaryIntegrals;
-addpath ../Examples;
+%addpath ../BoundaryIntegrals;
+%addpath ../Examples;
 addpath ../IncidentWaves;
-addpath ../IntegratorCompGauss;
-addpath ../IntegratorNSD;
-addpath ../QuadRoutines;
-addpath ../UtilityFunctions
+%addpath ../IntegratorCompGauss;
+% addpath ../IntegratorNSD;
+% addpath ../QuadRoutines;
+addpath ../UtilityFunctions;
+addpath ../Collocation;
 
 end
 
