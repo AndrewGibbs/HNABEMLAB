782c8bae8ff00254780d61aef5a9259cc7016a6b
Unknown <andrewgibbs1987@gmail.com>
2018-08-19 22:37:29 +0100

    increased RelTol for ODE45 of Levin

--- src/LevinQuick.m
+++ src/LevinQuick.m
@@ -3,7 +3,7 @@ function I = LevinQuick(a,b,f,G,k)
     %define the Levin ODE:
     Ly = @(x,y) f(x) -  1i*k*G{2}(x)*y;
     %solve it:
-    [~,y] = ode45(Ly,[a b],0,odeset('RelTol',1E-8));
+    [~,y] = ode45(Ly,[a b],0,odeset('RelTol',1E-12));
     %solution is anti-derivative, so use this to estiamate integral:
     I = y(end)*exp(1i*k*G{1}(b));%-y(1)*exp(1i*k*G{1}(a));
 end
