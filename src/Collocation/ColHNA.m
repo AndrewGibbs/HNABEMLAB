e8cc3a20b1c95b9783ac54738d4b4d84ef7ddf03
Unknown <andrewgibbs1987@gmail.com>
2019-01-03 18:20:30 +0000

    fixed the bug, but seem to have created loads of other problems...

--- src/Collocation/ColHNA.m
+++ src/Collocation/ColHNA.m
@@ -69,27 +69,28 @@ function [v_N, GOA, colMatrix, colRHS] = ColHNA(Operator, Vbasis, uinc, Gamma, v
     colMatrix=zeros(length(Xstruct),length(Vbasis.el));
     numColPts = length(Xstruct);
     numBasEls = length(Vbasis.el);
-    for m=1:numColPts
+    parfor m=1:numColPts
         VbasisCopy = Vbasis;
         fCopy = f;
         colMatrixCol = zeros(1,numBasEls);
         for n=1:numBasEls
         %manually do first entry of row
            if n==1
-               [colMatrixCol(n), quadData] = colEvalV2(Operator, VbasisCopy.el(1), VbasisCopy.elSide(1), Xstruct(m), Nquad,[], standardQuadFlag);
+               [colMatrixCol(n), quadData] = colEvalV3(Operator, VbasisCopy.el(1), VbasisCopy.elSide(1), Xstruct(m), Nquad,[], standardQuadFlag);
            elseif VbasisCopy.el(n).pm == VbasisCopy.el(n-1).pm && isequal(VbasisCopy.el(n).supp,VbasisCopy.el(n-1).supp)
                %reuse quadrature from previous iteration of this loop,
                %(phase and domain are the same)
-               colMatrixCol(n) = colEvalV2(Operator, VbasisCopy.el(n), VbasisCopy.elSide(n), Xstruct(m), Nquad, quadData, standardQuadFlag);
+               colMatrixCol(n) = colEvalV3(Operator, VbasisCopy.el(n), VbasisCopy.elSide(n), Xstruct(m), Nquad, quadData, standardQuadFlag);
            else
                %get fresh quadrature data
-               [colMatrixCol(n), quadData] = colEvalV2(Operator, VbasisCopy.el(n), VbasisCopy.elSide(n), Xstruct(m), Nquad,[], standardQuadFlag);
+               [colMatrixCol(n), quadData] = colEvalV3(Operator, VbasisCopy.el(n), VbasisCopy.elSide(n), Xstruct(m), Nquad,[], standardQuadFlag);
+               %colEvalV3(Operator, VbasisCopy.el(n), VbasisCopy.elSide(n), Xstruct(m), Nquad,[], standardQuadFlag);
            end
         end 
         colMatrix(m,:) = colMatrixCol;
         fX = fCopy.eval(Xstruct(m).x,Xstruct(m).side);
         if ~standardBEMflag
-           SPsiX = colEvalV2(Operator, GOA, GOA.illumSides, Ystruct(m), Nquad,[], standardQuadFlag);
+           SPsiX = colEvalV3(Operator, GOA, GOA.illumSides, Ystruct(m), Nquad,[], standardQuadFlag);
            colRHS(m)  = fX - SPsiX;
         else
            colRHS(m)  = fX;
